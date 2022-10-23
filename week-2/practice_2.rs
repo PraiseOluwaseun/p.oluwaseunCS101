0d7db2a402870cfe88c4e456b36ee4a6fcb7cf54
praise <praise.oluwaseun@pau.edu.ng>
2022-10-23 09:38:13 +0100

    (My second commit)

---
+++ week-2/practice_2.rs
@@ -0,0 +1,6 @@
+fn main() {
+	println!(); // print just a
+	println!("Hello "); //prints
+	println!("format {} arguments","some"); //prints format some arguments
+	println!("My name is {}. I am the a memmeber of stream 2  ", "Tishe Oluwaseun"); 
+}
