68a219072c6447a2d762b0947faf3635b2e9b333
PraiseOluwaseun <115806953+PraiseOluwaseun@users.noreply.github.com>
2022-11-16 19:49:40 +0100

    Delete week_5'/practice_8/part_2/target directory

--- week_5'/practice_8/part_2/target/debug/deps/part_2.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Shopinverse\Documents\p.oluwaseunCS101\week_5'\practice_8\part_2\target\debug\deps\part_2.exe: src\main.rs
-
-C:\Users\Shopinverse\Documents\p.oluwaseunCS101\week_5'\practice_8\part_2\target\debug\deps\part_2.d: src\main.rs
-
-src\main.rs:
--- week_5'/practice_8/part_2/target/debug/part_2.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Shopinverse\Documents\p.oluwaseunCS101\week_5'\practice_8\part_2\target\debug\part_2.exe: C:\Users\Shopinverse\Documents\p.oluwaseunCS101\week_5'\practice_8\part_2\src\main.rs
