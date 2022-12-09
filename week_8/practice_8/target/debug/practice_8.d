75e57c31d70bce18e9f3a3ccc4b9549b0df2647c
praise <praise.oluwaseun@pau.edu.ng>
2022-12-09 14:10:56 +0100

    (My commits)

---
+++ week_8/practice_8/target/debug/practice_8.d
@@ -0,0 +1,1 @@
+C:\Users\Shopinverse\Documents\p.oluwaseunCS101\week_8\practice_8\target\debug\practice_8.exe: C:\Users\Shopinverse\Documents\p.oluwaseunCS101\week_8\practice_8\src\main.rs
---
+++ week_8/practice_9/target/debug/practice_9.d
@@ -0,0 +1,1 @@
+C:\Users\Shopinverse\Documents\p.oluwaseunCS101\week_8\practice_9\target\debug\practice_9.exe: C:\Users\Shopinverse\Documents\p.oluwaseunCS101\week_8\practice_9\src\main.rs
