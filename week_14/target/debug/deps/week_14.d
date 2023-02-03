d7b5477b01c21d545292250a0e4d6876b99874af
praise <praise.oluwaseun@pau.edu.ng>
2023-02-03 15:26:59 +0100

    (Commits)

---
+++ week_14/target/debug/deps/week_14.d
@@ -0,0 +1,5 @@
+C:\Users\Shopinverse\Documents\p.oluwaseunCS101\week_14\target\debug\deps\week_14.exe: src\main.rs
+
+C:\Users\Shopinverse\Documents\p.oluwaseunCS101\week_14\target\debug\deps\week_14.d: src\main.rs
+
+src\main.rs:
---
+++ week_14/target/debug/week_14.d
@@ -0,0 +1,1 @@
+C:\Users\Shopinverse\Documents\p.oluwaseunCS101\week_14\target\debug\week_14.exe: C:\Users\Shopinverse\Documents\p.oluwaseunCS101\week_14\src\main.rs
